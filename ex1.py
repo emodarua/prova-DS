cf4542389dbf9b6e1564ccc5a6791820a17707f7
emodarua <emodaesquina@gmail.com>
2026-03-31 21:42:07 -0300

    teste

--- ex1.py
+++ ex1.py
@@ -1,1 +1,12 @@
 # exercicio 1
+nomeEscola = "etec"
+nomeAluno = "luiz"
+turma = "DS"
+
+print('-' * 50)
+print('nome do escola: ', nomeEscola)
+print('nome do aluno: ', nomeAluno)
+print('turma: ', turma)
+print('-' * 50)
+
+# teste
