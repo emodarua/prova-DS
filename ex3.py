11ae7039c3380484d7ee8fe9acb2dc0b875aeb58
emodarua <emodaesquina@gmail.com>
2026-03-31 21:54:12 -0300

    ex3

---
+++ ex3.py
@@ -0,0 +1,10 @@
+num1 = int(input('digite um numero: '))
+num2 = int(input('digite outro numero: '))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
