f854dbbdce2366367330387b9f4d59028e435e74
emodarua <emodaesquina@gmail.com>
2026-03-31 21:59:15 -0300

    ex4

---
+++ ex4.py
@@ -0,0 +1,7 @@
+nota1 = float(input("digite um numero: "))
+nota2 = float(input("digite um numero: "))
+nota3 = float(input("digite um numero: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f"a media é de: {media:.2f} " )
