7e5324b06afa79f082722504e180c20a1423d104
emodarua <emodaesquina@gmail.com>
2026-03-31 21:49:09 -0300

    ex2

---
+++ ex2.py
@@ -0,0 +1,14 @@
+nome = 'luiz'
+idade = 16
+altura = 1.80
+aluno_ativo = True
+
+print(nome)
+print(idade)
+print(altura)
+print(aluno_ativo)
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(aluno_ativo))
