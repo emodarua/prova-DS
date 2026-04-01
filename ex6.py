16246b51c55b911d886c29fc88f905c99455c8c7
emodarua <emodaesquina@gmail.com>
2026-03-31 22:26:03 -0300

    ex6

---
+++ ex6.py
@@ -0,0 +1,11 @@
+nomeBruto = input('Digite seu nome completo: ')
+
+nomeLimpo = nomeBruto.strip()   
+print(nomeLimpo)
+
+# maiusculo
+print(nomeLimpo.upper)
+
+# minusculo
+print(nomeLimpo.lower)
+
